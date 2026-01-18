a70d62631d4d829d80d434fab5f534ee8b83f256
Eliseu Martínez <elimarmon@ieseljust.com>
2026-01-18 22:03:58 +0100

    Afegir ruta post compraventas i categorias

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,22 +1,27 @@
 <?php
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\CompraVentaController;
 use Illuminate\Support\Facades\Route;
 
-//Rutas de productos
-// Obtener todos los productos (Tienda/Mapa)
+// Rutas de productos
+
 Route::get('/productos', [ProductoController::class, 'index']);
-
-// Obtener un producto específico por ID
+Route::post("/productos", [ProductoController::class, "store"]);
 Route::get('/productos/{id}', [ProductoController::class, 'show']);
-
-// Crear un producto (Agricultor)
-Route::post('/productos', [ProductoController::class, 'store']);
-
-// Actualizar stock o precio (Agricultor)
 Route::put('/productos/{id}', [ProductoController::class, 'update']);
 
-//Rutas de usuarios
+// Rutas de categorías
+
+Route::post("/categorias", [CategoriaController::class, "store"]);
+
+// Rutas de usuarios
+
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/register', [UserController::class, 'store']);
 Route::post('/login', [UserController::class, 'login']);
+
+// Rutas de compraventa
+
+Route::post("/compraventa/{producto}", [CompraVentaController::class, 'store']);
