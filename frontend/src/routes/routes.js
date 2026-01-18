968aac44c08974e5fe63c3daf2c6f82a9c97b6ec
Eliseu Martínez <elimarmon@ieseljust.com>
2026-01-18 22:22:56 +0100

    Afegir ruta Productos

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,23 +1,25 @@
-import login from '../views/login.vue';
-import PerfilUsuario from '../components/PerfilUsuario.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import login from "../views/login.vue";
+import PerfilUsuario from "../components/PerfilUsuario.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import Productos from "@/components/Productos.vue";
 
 const routes = [
+    { path: "/", name: "/", component: Productos },
     {
-        path: '/login',
-        name: 'login',
-        component: login
+        path: "/login",
+        name: "login",
+        component: login,
     },
     {
-        path: '/mapa',
-        name: 'mapa',
-        component: PerfilUsuario
-    }
+        path: "/mapa",
+        name: "mapa",
+        component: PerfilUsuario,
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
+    routes,
 });
 
 export default router;
