cfb285731d2ee74a8a7c23f83f81cbf36c042ac5
Eliseu Martínez <elimarmon@ieseljust.com>
2026-01-18 21:54:29 +0100

    Modificadas tablas categorias y productos

--- docker/database/base.sql
+++ docker/database/base.sql
@@ -16,27 +16,30 @@ CREATE TABLE usuarios (
 
 CREATE TABLE categorias (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_categoria VARCHAR(255) NOT NULL
+    nombre_categoria VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE productos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_categoria INT,
+    id_usuario INT NOT NULL,
     nombre_producto VARCHAR(255) NOT NULL,
     descripcion TEXT,
     precio DECIMAL(10, 2) NOT NULL,
     stock_total INT NOT NULL DEFAULT 0,
     stock_reserva INT NOT NULL DEFAULT 0,
-    stock_real INT NOT NULL DEFAULT 0,
+    stock_real INT AS (stock_total - stock_reserva) STORED,
     imagen VARCHAR(255),
     estado ENUM(
         'agotado',
-        'reservado',
         'disponible'
-    ),
+    ) DEFAULT 'disponible',
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_categoria) REFERENCES categorias (id)
+    FOREIGN KEY (id_categoria) REFERENCES categorias (id),
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
 );
 
 CREATE TABLE chats (
