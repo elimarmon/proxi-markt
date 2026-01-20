1049233f86ef16a2fa2f310f1efc40c431587fab
Inés Álvarez Calle <inesalvarezdaw@gmail.com>
2026-01-20 11:37:51 +0100

    Preparamos apartado mapa y chat para las siguientes fases y así finalizar las rutas para el nav

---
+++ frontend/src/components/mapa.vue
@@ -0,0 +1,70 @@
+<template>
+  <navbar></navbar>
+  <div class="contenedor-pagina">
+    <div id="contenedor-titulo">
+      <h1 class="titulo">Mapa</h1>
+      <p class="subtitulo">
+        Visualiza los puntos de entrega y productos
+      </p>
+    </div>
+
+    <div id="contenedor-mapa">
+        Apartado mapa
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+import navbar from "./nav.vue";
+
+
+const router = useRouter();
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Segoe UI', 'Arial';
+}
+
+body {
+  min-width: 400px;
+}
+
+.contenedor-pagina {
+  margin-top: 80px;
+  padding: 20px 50px;
+}
+
+#contenedor-titulo{
+  max-width: 90%;
+  margin: 40px auto 0 auto;
+}
+
+.titulo {
+  font-family: sans-serif;
+  color: #4ca626;
+  margin-bottom: 10px;
+  font-weight: bold;
+}
+
+.subtitulo {
+  font-family: sans-serif;
+  color: #666666;
+  margin-bottom: 20px;
+}
+
+#contenedor-mapa {
+  max-width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+}
+</style>
