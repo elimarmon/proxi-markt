bbdb4a517025dd3dd31847759bb4ceeb62f6da95
Eliseu Martínez <elimarmon@ieseljust.com>
2026-02-06 08:46:23 +0100

    terminado esqueleto de formulario de valoracion

--- frontend/src/components/ValoracionForm.vue
+++ frontend/src/components/ValoracionForm.vue
@@ -1,16 +1,30 @@
 <script setup>
-import axios from 'axios';
-import { reactive, onMounted } from 'vue';
+import { reactive } from 'vue';
 
+const emit = defineEmits(['enviar-valoracion']);
+
+const form = reactive({
+    valoracion: 0,
+    comentario: ''
+});
+
+const enviarValoracion = () => {
+    emit('enviar-valoracion', {
+        valoracion: form.valoracion,
+        comentario: form.comentario
+    });
+}
 
 </script>
 <template>
-    <form @submit.prevent="enviarValoracion">
-        <label for="valoracion">Puntuación:</label><br>
-        <input v-model="form.estrellas" id="valoracion" name="valoracion" type="number" step="0.5">
-        <br><br>
-        <label for="comentario">Mensaje:</label><br>
-        <textarea v-model="form.comentario" name="comentario" id="comentario"></textarea>
-        <button type="submit">Valorar</button>
-    </form>
+    <div>
+        <form @submit.prevent="enviarValoracion">
+            <label for="valoracion">Puntuación:</label><br>
+            <input v-model="form.valoracion" id="valoracion" name="valoracion" type="number" step="0.5">
+            <br><br>
+            <label for="comentario">Mensaje:</label><br>
+            <textarea v-model="form.comentario" name="comentario" id="comentario"></textarea>
+            <button type="submit">Valorar</button>
+        </form>
+    </div>
 </template>
