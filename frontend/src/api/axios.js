1c0546bae64e2610f0c7a108df1f49787d2679db
Eliseu Martínez <elimarmon@ieseljust.com>
2026-02-08 10:29:19 +0100

    recarga forzada para actualizar variables de useAuth

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -26,7 +26,7 @@ api.interceptors.response.use(
     (error) => {
         if (error.response && error.response.status === 401) {
             localStorage.removeItem("token");
-            router.push("/login");
+            window.location.href("/login"); 
         }
         return Promise.reject(error);
     },
