77a9b8a38bd82be75f6e713d41fc95afbcdd9848
David Camarena <davidcugamer23@gmail.com>
2026-02-10 15:34:06 +0100

    script de ejecucion

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,25 @@ set -e
 git fetch origin
 git reset --hard origin/despliegue
 
-# 2. Reiniciar contenedores
+# 2. Asegurar que el build no lea traefik (el archivo manual que hicimos antes)
+echo "traefik/" > .dockerignore
+
+# 3. Reiniciar contenedores
 cd produccion
 docker-compose down -v --remove-orphans
 docker-compose up -d --build
 
-# 3. Configurar Laravel (Esperamos a que MySQL arranque)
+# 4. Configurar Laravel
+echo "==> Instalando librerías y configurando..."
 sleep 15
+
+# ESTA ES LA LÍNEA CLAVE PARA ARREGLAR TU ERROR:
+docker exec proximarkt-backend composer install --no-dev --optimize-autoloader
+
+# Reparar permisos
 docker exec proximarkt-backend chown -R www-data:www-data storage bootstrap/cache
 docker exec proximarkt-backend chmod -R 775 storage bootstrap/cache
+
+# Migraciones
 docker exec proximarkt-backend php artisan migrate --force
+docker exec proximarkt-backend php artisan config:clear
