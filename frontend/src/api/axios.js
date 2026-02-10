ec3150b3bb6e41b05ea36e669410f1c3de2f5bb7
David Camarena <davidcugamer23@gmail.com>
2026-02-10 12:40:08 +0100

    canvi de la url del back

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://proximarkt.francecentral.cloudapp.azure.com",
+    baseURL: "https://proximarkt.francecentral.cloudapp.azure.com/api",
     headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
